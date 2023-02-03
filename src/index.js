63b32d664aa83b810f12a0cf0aaa1ee21a637b18
Blossom <saphashb@gmail.com>
2023-02-03 12:51:36 +0100

    create a user model

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
-import router from './routes/noteRoutes.js'
+import router from './routes/noteRoute.js'
 
 dotenv.config()
 
