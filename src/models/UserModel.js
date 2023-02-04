39e79048e191e299e770ce6315258c9cf2725c4c
Blossom <saphashb@gmail.com>
2023-02-04 21:13:50 +0100

    rename user route

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import isEmail from 'validator/lib/isEmail.js';
+
+const { SALT_ROUNDS, BCRYPT_PASSWORD } = process.env;
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -7,13 +11,32 @@ const UserSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true,
+    validate: [isEmail, 'invalid email'],
   },
   password: {
     type: String,
     required: true
   }
 })
+
+UserSchema.pre('save', async function save(next) {
+  console.log('hit')
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(String(SALT_ROUNDS));
+    console.log(salt, typeof salt)
+    this.password = bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+UserSchema.methods.validatePassword = async function validatePassword(data) {
+  return bcrypt.compare(data, this.password);
+};
 
 const User = mongoose.model('User', UserSchema)
 
